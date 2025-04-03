42e6c431398800ae1813de2a957bdebdffdbd438
Miguele73 <cabusam.eu@gmail.com>
2025-04-03 12:56:13 +0200

    Solved dirty_screen_2

--- lib/src/features/dirty_screen_2/presentation/dirty_screen_two.dart
+++ lib/src/features/dirty_screen_2/presentation/dirty_screen_two.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dirty_code/src/features/dirty_screen_2/presentation/dirty_box2.dart';
 import 'package:flutter/material.dart';
 
 class DirtyScreenTwo extends StatelessWidget {
@@ -8,86 +10,14 @@ class DirtyScreenTwo extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Dirty Screen #2"),
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.greenAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.eco),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Box A',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.greenAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.eco),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Box B',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.greenAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.eco),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Box C',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  color: Colors.greenAccent),
-              margin: const EdgeInsets.all(4),
-              padding: const EdgeInsets.all(16.0),
-              child: const Row(
-                children: [
-                  Icon(Icons.eco),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    'Box D',
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )
-                ],
-              ),
-            ),
+            DirtyBox2(letter: 'A'),
+            DirtyBox2(letter: 'B'),
+            DirtyBox2(letter: 'C'),
+            DirtyBox2(letter: 'D'),
           ],
         ),
       ),
